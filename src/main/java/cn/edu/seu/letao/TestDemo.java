4eb3516c4fad35cd00031551e22340c0e3e4ceec
lijia <2902889778@qq.com>
2020-03-20 13:14:09 +0800

    first commit

---
+++ src/main/java/cn/edu/seu/letao/TestDemo.java
@@ -0,0 +1,12 @@
+package cn.edu.seu.letao;
+
+/**
+ * @author shkstart
+ * @create 2020-03-20 12:50
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("===========");
+
+    }
+}
