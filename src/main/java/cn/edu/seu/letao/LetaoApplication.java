df9405a01dc2be8f8cf3be2baa36a0db7b95b8c2
BuRNIngsuperman <2502975747@qq.com>
2020-03-20 10:31:13 +0800

    first commit

---
+++ src/main/java/cn/edu/seu/letao/LetaoApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.seu.letao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/seu/letao/LetaoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.seu.letao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
