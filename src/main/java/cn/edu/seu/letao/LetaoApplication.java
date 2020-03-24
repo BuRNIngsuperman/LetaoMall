70883f330a7c8e0677692f572923e7cd9ffefe8b
Eoogle <331295045@qq.com>
2020-03-24 18:35:07 +0800

    同步到有mapperscan 删除test文件的版本

--- src/main/java/cn/edu/seu/letao/LetaoApplication.java
+++ src/main/java/cn/edu/seu/letao/LetaoApplication.java
@@ -1,9 +1,11 @@
 package cn.edu.seu.letao;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.edu.seu.letao.*.mapper")
 public class LetaoApplication {
 
     public static void main(String[] args) {
