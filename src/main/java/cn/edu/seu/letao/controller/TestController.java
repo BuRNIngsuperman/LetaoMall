1530ef5e46e2bb324270eea1b4795dc7b4b8beb2
Eoogle <331295045@qq.com>
2020-03-25 14:53:34 +0800

    修改login.html和register.html，增加mall模块其他需要的页面

---
+++ src/main/java/cn/edu/seu/letao/controller/TestController.java
@@ -0,0 +1,16 @@
+package cn.edu.seu.letao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class TestController {
+
+
+    @GetMapping({"/index", "/", "/index.html"})
+    public String indexPage(){
+        return "mall/index";
+    }
+}
