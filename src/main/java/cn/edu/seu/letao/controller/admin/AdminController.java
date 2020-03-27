23c10b42fdc23a8b49a54df39d6ae7721222862a
zhangwen <383683706@qq.com>
2020-03-27 16:37:08 +0800

    zhangwen

--- src/main/java/cn/edu/seu/letao/controller/admin/AdminController.java
+++ src/main/java/cn/edu/seu/letao/controller/admin/AdminController.java
@@ -1,6 +1,7 @@
 package cn.edu.seu.letao.controller.admin;
 
 import cn.edu.seu.letao.entity.UsrAccount;
+import cn.edu.seu.letao.service.admin.IAdminService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,14 +14,45 @@ import org.springframework.util.StringUtils;
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
+    IAdminService adminService;
+
     @GetMapping(path = {"","/","/index","index.html"})
     public String index(){
         return "admin/admin_index";
     }
 
-    @RequestMapping(value = "/login",method = {RequestMethod.POST,RequestMethod.GET})
-    public String login(){
+    @GetMapping(value = "/login")
+    public String toLogin(){
         return "admin/admin_login";
+    }
+
+    @PostMapping(value = "/login")
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        @RequestParam("verifyCode") String verifyCode,
+                        HttpSession session) {
+        if (StringUtils.isEmpty(verifyCode)) {
+            session.setAttribute("errorMsg", "验证码不能为空");
+            return "admin/admin_login";
+        }
+        if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
+            session.setAttribute("errorMsg", "用户名或密码不能为空");
+            return "admin/admin_login";
+        }
+        String kaptchaCode = session.getAttribute("verifyCode") + "";
+        if (StringUtils.isEmpty(kaptchaCode) || !verifyCode.equals(kaptchaCode)) {
+            session.setAttribute("errorMsg", "验证码错误");
+            return "admin/admin_login";
+        }
+        Boolean bool = adminService.checkAccount(username, password);
+        if (bool) {
+            session.setAttribute("loginUser", username);
+            return "redirect:/admin/index";
+        } else {
+            session.setAttribute("errorMsg", "登陆失败，没有此账户");
+            return "admin/admin_login";
+        }
     }
 
     @RequestMapping(value = "/register",method = {RequestMethod.POST,RequestMethod.GET})
