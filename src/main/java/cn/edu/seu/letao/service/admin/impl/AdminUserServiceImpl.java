e4116053e72150fe5b5240c89803651092a45d5e
BuRNIngsuperman <2502975747@qq.com>
2020-03-31 19:01:10 +0800

    会员管理模块

---
+++ src/main/java/cn/edu/seu/letao/service/admin/impl/AdminUserServiceImpl.java
@@ -0,0 +1,31 @@
+package cn.edu.seu.letao.service.admin.impl;
+
+import cn.edu.seu.letao.entity.UsrUser;
+import cn.edu.seu.letao.mapper.UsrUserMapper;
+import cn.edu.seu.letao.service.admin.IAdminUserService;
+import cn.edu.seu.letao.util.PageQueryUtil;
+import cn.edu.seu.letao.util.PageResult;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Jin Qiuyang
+ * @date 2020/3/31
+ */
+@Service
+public class AdminUserServiceImpl extends ServiceImpl<UsrUserMapper, UsrUser> implements IAdminUserService {
+
+    @Autowired
+    UsrUserMapper usrUserMapper;
+
+    @Override
+    public PageResult getMallUserPage(PageQueryUtil pageUtil) {
+        List<UsrUser> mallUserList = usrUserMapper.findMallUserList(pageUtil);
+        int total = usrUserMapper.getTotalMallUsersCount(pageUtil);
+        PageResult pageResult = new PageResult(mallUserList, total, pageUtil.getLimit(), pageUtil.getPage());
+        return pageResult;
+    }
+}
