b6b83908ace35bb75464ae0968ddf9363952fda3
BuRNIngsuperman <2502975747@qq.com>
2020-04-03 16:53:48 +0800

    数据统计报表更新

--- src/main/java/cn/edu/seu/letao/controller/admin/AdminStatisticsController.java
+++ src/main/java/cn/edu/seu/letao/controller/admin/AdminStatisticsController.java
@@ -1,9 +1,13 @@
 package cn.edu.seu.letao.controller.admin;
 
+import cn.edu.seu.letao.util.Result;
+import cn.edu.seu.letao.util.ResultGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -14,6 +18,16 @@ public class AdminStatisticsController {
     public String statisticsPage(Model model){
         model.addAttribute("path","admin_statistics");
         return "admin/admin_statistics";
+    }
+
+    @RequestMapping(value = "/statistics/order",method = {RequestMethod.GET})
+    @ResponseBody
+    public Result orderCount(@RequestParam int type,
+                             @RequestParam(required = false) String startTime,
+                             @RequestParam(required = false) String endTime,
+                             @RequestParam(required = false) int year){
+
+        return ResultGenerator.genSuccessResult();
     }
 
 
---
+++ src/main/resources/static/admin/dist/js/letao_mall_statistics.js
@@ -0,0 +1,114 @@
+$(document).ready(function () {
+    $("#year").append("<option selected value='-1'>按年统计</option>")
+    var startYear = 2018,nowYear = new Date().getFullYear();
+    for(var  i = 0;i<=nowYear-startYear+1;i++){
+        $("#year").append("<option value='"+startYear+"'>"+startYear+"年</option>");
+        startYear += 1;
+    }
+});
+
+//基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById('container'),'light');
+var xDatas=[],yDatas=[],startDate,endDate,year=0;
+
+getChartData(0,null,null);
+function getChartData(type) {
+    var index = layer.load(3);
+    $.ajax({
+        type: 'GET',
+        url: '/admin/statistics/order',
+        dataType: 'json',
+        data:{
+            type: type,
+            startTime: startDate,
+            endTime: endDate,
+            year: year
+        },
+        success: function(r){
+            layer.close(index);
+            if(r.resultCode != 200){
+                layer.alert(data.message,{title: '错误信息',icon: 2});
+                return;
+            }
+            console.log(data)
+            xDatas = r.result.xDatas;
+            yDatas = r.result.yDatas;
+            $("#countAll").html(r.result.countAll);
+            drawChart();
+        },
+        error:function(XMLHttpRequest){
+            layer.close(index);
+            layer.alert('数据处理失败! 错误码:'+XMLHttpRequest.status,{title: '错误信息',icon: 2});
+        }
+    });
+}
+
+function getCustomData() {
+    startDate = $('#startDate').val();
+    if(startDate==""||startDate==null){
+        $('#startDate').focus();
+        return;
+    }
+    endDate = $('#endDate').val();
+    if(endDate==""||endDate==null){
+        $('#endDate').focus();
+        return;
+    }
+    getChartData(-1);
+}
+
+function getYearData() {
+    console.log(1)
+    year = $('#year').val();
+    if(year==""||year=="-1"||year==null){
+        return;
+    }
+    getChartData(-2);
+}
+
+
+function drawChart() {
+    // 指定图表的配置项和数据
+    var option = {
+        title: {
+            text: '订单销量统计',
+            subtext: 'XMall'
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['销售金额']
+        },
+        toolbox: {
+            show: true,
+            feature: {
+                dataZoom: {
+                    yAxisIndex: 'none'
+                },
+                dataView: {readOnly: false},
+                magicType: {type: ['line', 'bar']},
+                restore: {},
+                saveAsImage: {}
+            }
+        },
+        xAxis: {
+            boundaryGap: false,
+            data: xDatas
+        },
+        yAxis: {
+            type: 'value',
+            axisLabel: {
+                formatter: '{value} ￥'
+            }
+        },
+        series: [{
+            name: '销售金额',
+            type: 'line',
+            data: yDatas
+        }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+}
