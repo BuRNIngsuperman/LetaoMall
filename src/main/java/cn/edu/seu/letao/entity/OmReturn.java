7169c1c468c859ddb0cae17832f6ab271c908987
zhangwen <383683706@qq.com>
2020-04-07 18:26:43 +0800

    zw

---
+++ src/main/java/cn/edu/seu/letao/entity/OmReturn.java
@@ -0,0 +1,73 @@
+package cn.edu.seu.letao.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.time.LocalDateTime;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Hala
+ * @since 2020-04-07
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class OmReturn implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 唯一标识id
+     */
+    @TableId(value = "return_id", type = IdType.AUTO)
+    private Integer returnId;
+
+    /**
+     * 订单id
+     */
+    private String orderSn;
+
+    /**
+     * 申请创建时间
+     */
+    private LocalDateTime createdTime;
+
+    /**
+     * 退货数量
+     */
+    private Integer returnCount;
+
+    /**
+     * 退货人姓名
+     */
+    private String returnName;
+
+    /**
+     * 退货人电话
+     */
+    private String returnPhone;
+
+    /**
+     * 申请状态
+     */
+    private Integer status;
+
+    /**
+     * 申请原因
+     */
+    private String reason;
+
+    /**
+     * 退货商品条码
+     */
+    private String productSn;
+
+
+}
---
+++ src/main/java/cn/edu/seu/letao/mapper/OmReturnMapper.java
@@ -0,0 +1,16 @@
+package cn.edu.seu.letao.mapper;
+
+import cn.edu.seu.letao.entity.OmReturn;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Hala
+ * @since 2020-04-07
+ */
+public interface OmReturnMapper extends BaseMapper<OmReturn> {
+
+}
