cbc2a90ec58f7ab5d72503ab1826ca0a32f6e950
zhangwen <383683706@qq.com>
2020-04-07 16:06:55 +0800

    zw

---
+++ src/main/java/cn/edu/seu/letao/controller/vo/OrderItemVO.java
@@ -0,0 +1,31 @@
+package cn.edu.seu.letao.controller.vo;
+
+public class OrderItemVO {
+    String goodsName;
+    int goodsCount;
+    String goodsSN;
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public int getGoodsCount() {
+        return goodsCount;
+    }
+
+    public void setGoodsCount(int goodsCount) {
+        this.goodsCount = goodsCount;
+    }
+
+    public String getGoodsSN() {
+        return goodsSN;
+    }
+
+    public void setGoodsSN(String goodsSN) {
+        this.goodsSN = goodsSN;
+    }
+}
